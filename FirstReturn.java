bff177ba325ea03c40b17a754b39aa45620e3855
Strangeur <rgamon1225@Gmail.com>
2025-10-25 18:28:11 +0800

    Good bye :steamisSad:

--- FirstReturn.java
+++ FirstReturn.java
@@ -1,5 +1,13 @@
 public class FirstReturn {
     public static void main (String[] args) {
         System.out.println("Hello :steamhappy:");
+
+
+
+        System.out.println("Goodbye :steamsad:"); //First Update
+
+
+
+
     }
 }
